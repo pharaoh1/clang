1bfe4d7c00b4d4754cf1e0bce5f693c6158b1915
Nick Desaulniers <ndesaulniers@google.com>
2018-09-20 15:25:52 -0700

    also run test on gnu99+

--- test/Sema/gnu89-const.c
+++ test/Sema/gnu89-const.c
@@ -1,17 +1,24 @@
-// RUN: %clang_cc1 %s -std=gnu89 -pedantic -fsyntax-only 2>&1 | FileCheck -check-prefix=CHECK-PEDANTIC %s
 // RUN: %clang_cc1 %s -std=gnu89 -fsyntax-only 2>&1 | FileCheck -check-prefix=CHECK-ABSTRUSE %s
+// RUN: %clang_cc1 %s -std=gnu89 -pedantic -fsyntax-only 2>&1 | FileCheck -check-prefix=CHECK-GNU89-PEDANTIC %s
+// RUN: %clang_cc1 %s -std=gnu99 -fsyntax-only 2>&1 | FileCheck -check-prefix=CHECK-ABSTRUSE %s
+// RUN: %clang_cc1 %s -std=gnu99 -pedantic -fsyntax-only 2>&1 | FileCheck -check-prefix=CHECK-ABSTRUSE %s
+// RUN: %clang_cc1 %s -std=gnu11 -fsyntax-only 2>&1 | FileCheck -check-prefix=CHECK-ABSTRUSE %s
+// RUN: %clang_cc1 %s -std=gnu11 -pedantic -fsyntax-only 2>&1 | FileCheck -check-prefix=CHECK-ABSTRUSE %s
+// RUN: %clang_cc1 %s -std=gnu17 -fsyntax-only 2>&1 | FileCheck -check-prefix=CHECK-ABSTRUSE %s
+// RUN: %clang_cc1 %s -std=gnu17 -pedantic -fsyntax-only 2>&1 | FileCheck -check-prefix=CHECK-ABSTRUSE %s
 
 // Do not warn about duplicate const declaration specifier as the result of
-// typeof in gnu89.
+// typeof in gnu89, unless -pedantic was specified. Do not warn in gnu99+, even
+// with -pedantic.
 const int c_i;
 const typeof(c_i) c_i2;
-// CHECK-PEDANTIC: 7:7: warning: extension used
-// CHECK-PEDANTIC: 7:1: warning: duplicate 'const' declaration specifier
-// CHECK-ABSTRUSE-NOT: 7:1: warning: duplicate 'const' declaration specifier
+// CHECK-GNU89-PEDANTIC: 14:7: warning: extension used
+// CHECK-GNU89-PEDANTIC: 14:1: warning: duplicate 'const' declaration specifier
+// CHECK-ABSTRUSE-NOT: 14:1: warning: duplicate 'const' declaration specifier
 
 const const int c_i3;
-// CHECK: 12:7: warning: duplicate 'const' declaration specifier
+// CHECK: 19:7: warning: duplicate 'const' declaration specifier
 
 typedef const int t;
 const t c_i4;
-// CHECK: 16:1: warning: duplicate 'const' declaration specifier
+// CHECK: 23:1: warning: duplicate 'const' declaration specifier
